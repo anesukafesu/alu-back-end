74db39734a3eeb041a19a0c1e0185d00b0740387
anesu <anesukafesu@gmail.com>
2023-07-06 16:40:10 +0300

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,7 +12,7 @@ def get_data(url):
     """
     request = get(url)
 
-    if request.status_code == 200:
+    if request.get('status_code') == 200:
         try:
             return request.json()
         except:
@@ -27,7 +27,7 @@ def main():
 
     # Getting the name
     user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    name = get_data(user_url)["name"]
+    name = get_data(user_url).get('name')
 
     # Getting the todos
     tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
@@ -37,15 +37,15 @@ def main():
     number_of_completed_todos = 0
 
     for todo in todos:
-        if todo["completed"]:
+        if todo.get('completed'):
             number_of_completed_todos += 1
 
     print(
         f'Employee {name} is done with tasks({number_of_completed_todos}/{total_number_of_todos})')
 
     for todo in todos:
-        if todo["completed"]:
-            print(f'\t {todo["title"]}')
+        if todo.get('completed'):
+            print(f'\t {todo.get("title")}')
 
 
 if __name__ == "__main__":

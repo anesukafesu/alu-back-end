f49e1a8b00953633eaced837c2561a1d32b6505b
anesu <anesukafesu@gmail.com>
2023-07-15 12:53:54 +0300

    3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,8 +8,8 @@ from json import dump
 
 def get_data(url):
     """gets data from an api"""
-    request = get(url)
-    
+    request = get(url, verify=False)
+
     if request.status_code == 200:
         return request.json()
     else:
@@ -20,18 +20,18 @@ def main():
     """program starting point"""
 
     # Get users
-    users_data_url = f'https://jsonplaceholder.typicode.com/users'
+    users_data_url = 'https://jsonplaceholder.typicode.com/users'
     users = get_data(users_data_url)
 
     # Create a user hashmap for faster lookup
     users_hashmap = {}
     for user in users:
-        user_id = user['id']
-        username = user['username']
+        user_id = user.get('id')
+        username = user.get('username')
         users_hashmap[user_id] = username
 
     # Get todos
-    todos_url = f'https://jsonplaceholder.typicode.com/todos'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
     todos = get_data(todos_url)
 
     # Data object to write
@@ -40,13 +40,19 @@ def main():
     # Add todos to data object
     for todo in todos:
         user_id = todo["userId"]
-        task_data = {'username': users_hashmap[user_id], 'task': todo['title'], 'completed': todo['completed']}
+        task_data = { 
+            'username': users_hashmap.get(user_id),
+            'task': todo.get('title'),
+            'completed': todo.get('completed')
+        }
 
         if not user_id in data:
             data[user_id] = []
-        
-        data[user_id].append(task_data)
-    
+
+        user_todos = data[user_id]
+        user_todos.append(task_data)
+        data[user_id] = user_todos
+
     with open(f'todo_all_employees.json', 'w') as f:
         dump(data, f)
 

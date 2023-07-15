b9a7b56e3dda83c937e416b036ec75f6b09daabf
anesu <anesukafesu@gmail.com>
2023-07-15 12:29:50 +0300

    1

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -9,8 +9,8 @@ from sys import argv
 
 def get_data(url):
     """gets data from an api"""
-    request = get(url)
-    
+    request = get(url, verify=False)
+
     if request.status_code == 200:
         return request.json()
     else:
@@ -20,22 +20,30 @@ def get_data(url):
 def main():
     """program starting point"""
     user_id = argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com'
 
     # Get user data
-    user_data_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    username = get_data(user_data_url)["username"]
+    user_data_url = base_url + '/users/{}'.format(user_id)
+    username = get_data(user_data_url).get('username')
 
     # Get todos
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
+    todos_url = base_url + '/todos?userId={}'.format(user_id)
     todos = get_data(todos_url)
 
     # Data object to write
     data = {}
-    data[user_id] = []
+    todos_to_write = []
 
     for todo in todos:
-        data[user_id].append({'username': username, 'task': todo['title'], 'completed': todo['completed']})
-    
+        todo_to_write = {
+            'username': username,
+            'task': todo.get('title'),
+            'completed': todo.get('completed')
+        }
+        todos_to_write.append(todo_to_write)
+
+    data[user_id] = todos_to_write
+
     with open(f'{user_id}.json', 'w') as f:
         dump(data, f)
 
